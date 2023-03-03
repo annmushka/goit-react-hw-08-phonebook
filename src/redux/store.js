5fe17b4cf57dd3e5c84b11c9000f55bb2641fd46
Ann <ann.mushka@gmail.com>
2023-03-03 10:32:47 +0200

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,55 @@
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import { contactsReducer } from './contactSlice.jsx';
+// import { userReducer } from './userSlice.js';
+// import storage from 'redux-persist/lib/storage';
+
+// export const rootReducer = combineReducers({
+//   contactsData: contactsReducer,
+//   user: userReducer,
+// });
+
+import { contactsReducer } from './contactSlice';
+import { userReducer } from './userSlice';
+
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactSlice.jsx';
-import { userReducer } from './userSlice.js';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const contactsReducerConfig = {
+  key: 'user',
+  storage,
+  blacklist: ['filter'],
+};
 
 export const store = configureStore({
   reducer: {
-    contactsData: contactsReducer,
-    user: userReducer,
+    contactsData: persistReducer(contactsReducerConfig, contactsReducer),
+    auth: persistReducer(contactsReducerConfig, userReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          persistStore,
+          persistReducer,
+          FLUSH,
+          PAUSE,
+          PERSIST,
+          REHYDRATE,
+          PURGE,
+          REGISTER,
+        ],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
